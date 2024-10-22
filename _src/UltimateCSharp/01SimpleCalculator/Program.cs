61000d5109ba9054db4c00d7baa3f86c9a8ecdf6
dilson.manjenje <dilson.manjenje@tsco.co.ao>
2024-10-22 11:58:52 +0100

    Section 1 - Initial Commit

---
+++ _src/UltimateCSharp/01SimpleCalculator/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("Input first number:");
+int firstNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Input second number:");
+int secondNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("What do you want to do with those numbers?");
+Console.WriteLine("[A]dd");
+Console.WriteLine("[S]ubtract");
+Console.WriteLine("[M]ultiply");
+
+string operacao = Console.ReadLine().ToUpper();
+
+string result = string.Empty;
+
+if (operacao == "A")
+	result = $"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}";
+else if (operacao == "S")
+	result = $"{firstNumber} - {secondNumber} = {firstNumber - secondNumber}";
+else if (operacao == "M")
+	result = $"{firstNumber} * {secondNumber} = {firstNumber * secondNumber}";
+else
+	result = "Invalid option";
+
+Console.WriteLine(result);
+
+
+Console.WriteLine("Press Any Key to Close.");
+Console.ReadKey();
---
+++ _src/UltimateCSharp/TodoList/Program.cs
@@ -0,0 +1,51 @@
+﻿
+Console.WriteLine("Olá!");
+
+bool isValid = false;
+do
+{
+	MostrarOpcoes();
+
+	string opcaoSelecionada = Console.ReadLine();
+	isValid = IsOpcaoValida(opcaoSelecionada.ToUpper());
+
+	if (isValid)
+		Console.WriteLine($"Opção Selecionada: {opcaoSelecionada}");
+
+} while (!isValid);
+
+static void MostrarOpcoes()
+{
+	Console.WriteLine("");
+	Console.ForegroundColor = ConsoleColor.Green;
+	Console.WriteLine("O que deseja fazer?");
+	Console.ResetColor();
+
+	Console.WriteLine("");
+
+	Console.WriteLine("[V] Ver todas tarefas");
+	Console.WriteLine("[A] Adicionar tarefa");
+	Console.WriteLine("[E] Excluir tarefa");
+	Console.WriteLine("[S] Sair");
+	Console.WriteLine("");
+}
+
+static bool IsOpcaoValida(string opcaoSelecionada)
+{
+	var opcoes = new List<string>() { "V", "A", "E", "S" };
+
+	return opcoes.IndexOf(opcaoSelecionada) >= 0;
+}
+
+static int Factorial(int number)
+{
+	int result = 0;
+	if (number == 0 || number == 1) return 1; 
+
+	for (int i = 1; number > i; i++)
+	{
+		result *= i;
+	}
+}
+
+Console.ReadKey();
