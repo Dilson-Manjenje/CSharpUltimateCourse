07e53eb1d2e50402b63f93a60287a7fd5bc41f81
dilson.manjenje <dilson.manjenje@tsco.co.ao>
2024-10-23 14:45:42 +0100

    01 Assignment: Todo List App

--- _src/UltimateCSharp/TodoList/Program.cs
+++ _src/UltimateCSharp/TodoList/Program.cs
@@ -1,51 +1,130 @@
 ﻿
-Console.WriteLine("Olá!");
-
-bool isValid = false;
-do
+public class Program
 {
-	MostrarOpcoes();
+	private List<string> _tarefas { get; set; } = new List<string>();
 
-	string opcaoSelecionada = Console.ReadLine();
-	isValid = IsOpcaoValida(opcaoSelecionada.ToUpper());
-
-	if (isValid)
-		Console.WriteLine($"Opção Selecionada: {opcaoSelecionada}");
-
-} while (!isValid);
-
-static void MostrarOpcoes()
-{
-	Console.WriteLine("");
-	Console.ForegroundColor = ConsoleColor.Green;
-	Console.WriteLine("O que deseja fazer?");
-	Console.ResetColor();
-
-	Console.WriteLine("");
-
-	Console.WriteLine("[V] Ver todas tarefas");
-	Console.WriteLine("[A] Adicionar tarefa");
-	Console.WriteLine("[E] Excluir tarefa");
-	Console.WriteLine("[S] Sair");
-	Console.WriteLine("");
-}
-
-static bool IsOpcaoValida(string opcaoSelecionada)
-{
-	var opcoes = new List<string>() { "V", "A", "E", "S" };
-
-	return opcoes.IndexOf(opcaoSelecionada) >= 0;
-}
-
-static int Factorial(int number)
-{
-	int result = 0;
-	if (number == 0 || number == 1) return 1; 
-
-	for (int i = 1; number > i; i++)
+	public static void Main(string[] args)
 	{
-		result *= i;
+		var tarefas = new List<string>();
+		Console.WriteLine("Olá!");
+
+		string opcao = string.Empty;	
+		do
+		{
+			MostrarOpcoes();
+
+			opcao = Console.ReadLine().ToUpper();
+
+			switch (opcao)
+			{
+				case "V":
+				case "v":
+					ListarTarefas(tarefas);
+					break;
+				case "A":
+				case "a":
+					var descricao = string.Empty;
+					do
+					{
+						Console.WriteLine("Descrição da Tarefa:");
+						descricao = Console.ReadLine().Trim();
+
+						if (tarefas.IndexOf(descricao.ToLower()) >= 0)
+						{
+							Console.WriteLine("Tarefa já existe");
+							break;
+						}
+						
+						if(!string.IsNullOrEmpty(descricao))
+							tarefas.Add(descricao.Trim());
+
+					} while (string.IsNullOrWhiteSpace(descricao));
+					break;
+				case "E":
+				case "e":
+					Console.WriteLine("Indique o número da Tarefa a Excluir:");
+					var tarefaId = int.Parse(Console.ReadLine());
+
+					for (int idx = 0; idx < tarefas.Count; idx++)
+					{
+						if(idx == tarefaId)
+						{
+							var tarefaExcluida = tarefas[idx]; 
+							tarefas.Remove(tarefas[idx]);
+							Console.WriteLine($"Tarefa '{tarefaExcluida}' excluída!");
+						}
+					}
+
+					break;
+				case "S":
+				case "s":
+					opcao = "S";
+					break;
+				default:
+					Console.WriteLine("Opção Inválida!!!");
+					break;
+			}
+
+		} while ( opcao != "S");
+
+		Console.Write("Pressione qualquer tecla para finalizar...");
+		Console.ReadKey();
+
+	}
+
+	private static void AddTarefa(List<string> tarefas)
+	{
+		var descricao = string.Empty;
+		do
+		{
+			Console.WriteLine("Descrição da Tarefa:");
+			descricao = Console.ReadLine();
+
+			if (tarefas.IndexOf(descricao.ToLower()) >= 0)
+				Console.WriteLine("Tarefa já existe");
+			else
+				tarefas.Add(descricao);
+
+		} while (string.IsNullOrWhiteSpace(descricao));
+
+		
+	}
+
+	private static void MostrarOpcoes()
+	{
+		Console.WriteLine("");
+		Console.ForegroundColor = ConsoleColor.Green;
+		Console.WriteLine("O que deseja fazer?");
+		Console.ResetColor();
+
+		Console.WriteLine("");
+
+		Console.WriteLine("[V] Ver todas tarefas");
+		Console.WriteLine("[A] Adicionar tarefa");
+		Console.WriteLine("[E] Excluir tarefa");
+		Console.WriteLine("[S] Sair");
+		Console.WriteLine("");
+	}
+
+	private static bool IsOpcaoValida(string opcaoSelecionada)
+	{
+		var opcoes = new List<string>() { "V", "A", "E", "S" };
+
+		return opcoes.IndexOf(opcaoSelecionada) >= 0;
+	}
+
+	private static void ExecutarOperacao(string opcao)
+	{
+		
+	}
+
+	private static void ListarTarefas(List<string> tarefas)
+	{ 
+		var idx = 0;
+		foreach (var tarefa in tarefas)
+		{
+			Console.WriteLine($"{idx} - {tarefa}");
+			++idx;
+		}
 	}
 }
-
-Console.ReadKey();
