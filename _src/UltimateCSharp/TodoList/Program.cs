10e319d7ca16515a68ffa3f83f4fb6cd1ae47e83
dilson.manjenje <dilson.manjenje@outlook.com>
2024-11-07 15:01:53 +0100

    Refactorar TODO App

--- _src/UltimateCSharp/TodoList/Program.cs
+++ _src/UltimateCSharp/TodoList/Program.cs
@@ -1,130 +1,109 @@
-﻿
-public class Program
+﻿var _tarefas = new List<string>();
+
+Console.WriteLine("Olá!");
+
+string opcao = string.Empty;
+do
 {
-	private List<string> _tarefas { get; set; } = new List<string>();
+	MostrarOpcoes();
 
-	public static void Main(string[] args)
+	opcao = Console.ReadLine().ToUpper();
+
+	switch (opcao)
 	{
-		var tarefas = new List<string>();
-		Console.WriteLine("Olá!");
+		case "V":
+			ListarTarefas();
+			break;
+		case "A":
+			AddTarefa();
+			break;
+		case "E":
+			ExcluirTarefa();
+			break;
+		case "S":
+			opcao = "S";
+			break;
+		default:
+			Console.WriteLine("Opção Inválida!!!");
+			break;
+	}
 
-		string opcao = string.Empty;	
-		do
+} while (opcao != "S");
+
+Console.Write("Pressione qualquer tecla para finalizar...");
+Console.ReadKey();
+
+void MostrarOpcoes()
+{
+	Console.WriteLine("");
+	Console.ForegroundColor = ConsoleColor.Green;
+	Console.WriteLine("O que deseja fazer?");
+	Console.ResetColor();
+
+	Console.WriteLine("");
+
+	Console.WriteLine("[V] Ver todas tarefas");
+	Console.WriteLine("[A] Adicionar tarefa");
+	Console.WriteLine("[E] Excluir tarefa");
+	Console.WriteLine("[S] Sair");
+	Console.WriteLine("");
+}
+
+void ListarTarefas()
+{
+	var idx = 1;
+	foreach (var tarefa in _tarefas)
+	{
+		Console.WriteLine($"{idx} - {tarefa}.");
+		++idx;
+	}
+}
+
+void AddTarefa()
+{
+	var descricao = string.Empty;
+	do
+	{
+		Console.WriteLine("Tarefa Descrição:");
+		descricao = Console.ReadLine().Trim();
+
+	} while (!IsDescricaoValida(descricao));
+
+	_tarefas.Add(descricao.Trim());
+}
+
+bool IsDescricaoValida(string descricao)
+{
+	if (string.IsNullOrWhiteSpace(descricao))
+	{
+		Console.WriteLine("Descrição não pode ser vázia");
+		return false;
+	}
+
+	if (_tarefas.Contains(descricao))
+	{
+		Console.WriteLine("Descrição deve ser única.");
+		return false;
+	}
+
+	return true;
+}
+
+void ExcluirTarefa()
+{
+	Console.WriteLine("Tarefa ID:");
+	var tarefaId = int.Parse(Console.ReadLine());
+	--tarefaId; 
+
+	for (int idx = 0; idx < _tarefas.Count; idx++)
+	{
+		if (idx == tarefaId)
 		{
-			MostrarOpcoes();
-
-			opcao = Console.ReadLine().ToUpper();
-
-			switch (opcao)
-			{
-				case "V":
-				case "v":
-					ListarTarefas(tarefas);
-					break;
-				case "A":
-				case "a":
-					var descricao = string.Empty;
-					do
-					{
-						Console.WriteLine("Descrição da Tarefa:");
-						descricao = Console.ReadLine().Trim();
-
-						if (tarefas.IndexOf(descricao.ToLower()) >= 0)
-						{
-							Console.WriteLine("Tarefa já existe");
-							break;
-						}
-						
-						if(!string.IsNullOrEmpty(descricao))
-							tarefas.Add(descricao.Trim());
-
-					} while (string.IsNullOrWhiteSpace(descricao));
-					break;
-				case "E":
-				case "e":
-					Console.WriteLine("Indique o número da Tarefa a Excluir:");
-					var tarefaId = int.Parse(Console.ReadLine());
-
-					for (int idx = 0; idx < tarefas.Count; idx++)
-					{
-						if(idx == tarefaId)
-						{
-							var tarefaExcluida = tarefas[idx]; 
-							tarefas.Remove(tarefas[idx]);
-							Console.WriteLine($"Tarefa '{tarefaExcluida}' excluída!");
-						}
-					}
-
-					break;
-				case "S":
-				case "s":
-					opcao = "S";
-					break;
-				default:
-					Console.WriteLine("Opção Inválida!!!");
-					break;
-			}
-
-		} while ( opcao != "S");
-
-		Console.Write("Pressione qualquer tecla para finalizar...");
-		Console.ReadKey();
-
-	}
-
-	private static void AddTarefa(List<string> tarefas)
-	{
-		var descricao = string.Empty;
-		do
-		{
-			Console.WriteLine("Descrição da Tarefa:");
-			descricao = Console.ReadLine();
-
-			if (tarefas.IndexOf(descricao.ToLower()) >= 0)
-				Console.WriteLine("Tarefa já existe");
-			else
-				tarefas.Add(descricao);
-
-		} while (string.IsNullOrWhiteSpace(descricao));
-
-		
-	}
-
-	private static void MostrarOpcoes()
-	{
-		Console.WriteLine("");
-		Console.ForegroundColor = ConsoleColor.Green;
-		Console.WriteLine("O que deseja fazer?");
-		Console.ResetColor();
-
-		Console.WriteLine("");
-
-		Console.WriteLine("[V] Ver todas tarefas");
-		Console.WriteLine("[A] Adicionar tarefa");
-		Console.WriteLine("[E] Excluir tarefa");
-		Console.WriteLine("[S] Sair");
-		Console.WriteLine("");
-	}
-
-	private static bool IsOpcaoValida(string opcaoSelecionada)
-	{
-		var opcoes = new List<string>() { "V", "A", "E", "S" };
-
-		return opcoes.IndexOf(opcaoSelecionada) >= 0;
-	}
-
-	private static void ExecutarOperacao(string opcao)
-	{
-		
-	}
-
-	private static void ListarTarefas(List<string> tarefas)
-	{ 
-		var idx = 0;
-		foreach (var tarefa in tarefas)
-		{
-			Console.WriteLine($"{idx} - {tarefa}");
-			++idx;
+			var tarefaExcluida = _tarefas[idx];
+			_tarefas.Remove(_tarefas[idx]);
+			Console.WriteLine($"Tarefa '{tarefaExcluida}' excluída!");
+			break;
 		}
 	}
 }
+
